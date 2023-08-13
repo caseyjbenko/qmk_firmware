78c98f32f58710def7ca7a4dad6c27cb83214ed9
Casey Benko <casey.benko@bltgv.com>
2023-08-13 18:13:07 -0400

    fn on hold space

--- keyboards/system76/launch_1/keymaps/casey/keymap.c
+++ keyboards/system76/launch_1/keymaps/casey/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGDN,
       KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,          KC_END,
       KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_RSFT,   KC_UP,
-      KC_LCTL,   KC_LALT, MO(2),   KC_LGUI, KC_SPC,           KC_SPC,           KC_RCTL, KC_RALT, TO(1),           KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL,   KC_LALT, MO(2),   KC_LGUI, LT(2,KC_SPC),     KC_SPC,           KC_RCTL, KC_RALT, TO(1),           KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   // Add homerow mod keys
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-      KC_ESC,    GUI_A, ALT_S, SFT_D, CTL_F, KC_TRNS, KC_TRNS, CTL_J, SFT_K, ALT_L, GUI_SCLN, KC_TRNS, KC_TRNS,         KC_TRNS,
+      KC_ESC,   GUI_A, ALT_S, SFT_D, CTL_F, KC_TRNS, KC_TRNS, CTL_J, SFT_K, ALT_L, GUI_SCLN, KC_TRNS, KC_TRNS,         KC_TRNS,
       KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,   KC_TRNS,
       KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,        KC_TRNS, KC_TRNS, TO(0),           KC_TRNS, KC_TRNS, KC_TRNS
   ),
